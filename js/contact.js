33a0e8f124c105e7f0ba2713c2b27bf250a5238a
Yassir <atertay@gmail.com>
2024-09-11 12:24:02 +0100

    update

--- js/contact.js
+++ js/contact.js
@@ -4,10 +4,25 @@ const userName = document.getElementById('username');
 const userEmail = document.getElementById('email');
 const msgArea = document.getElementById('msg');
 
-submitBtn.addEventListener('click', () => {
-  setTimeout(() => {
-    userName.value = userEmail.value = msgArea.value = '';
-  }, 2000);
-});
+submitBtn.onclick = function () {
+  if (userName.value != '' && userEmail.value != '' && msgArea.value != '') {
+    setTimeout(() => {
+      this.innerHTML = '<div class="loader"></div>';
+    }, 1000);
+    setTimeout(() => {
+      this.innerHTML = 'Message Sent!';
+      this.style =
+        'background-color: #fff; border:1px solid #646cff; color: #646cff; font-weight: 600;';
+    }, 3000);
+  }
+
+  if (userName.value == '') {
+    alert('enter your name');
+  } else if (userEmail.value == '') {
+    alert('enter your email');
+  } else if (msgArea.value == '') {
+    alert('enter your message');
+  }
+};
 
 //form
